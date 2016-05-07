549f71dbbf75f968a25e9b4029c38da8b3150e5a
Caio Almeida <caio.f.r.amd@gmail.com>
2016-05-07 16:11:23 -0300

    exemplo request simples

---
+++ blackfire.sh
@@ -0,0 +1,1 @@
+blackfire curl -H "Content-Type: application/json" -X POST -d '{"numero": "5318706731761894","titular": "Caio Almeida","cvv": "123","validade": "10/19"}' http://localhost/blackfire/api/
